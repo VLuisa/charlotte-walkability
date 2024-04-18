2a3f81216932f5b387409577a2c8a4c0fb47019f
Luisa Vasquez <luna.v97@gmail.com>
2024-04-18 18:27:04 -0400

    config file

---
+++ src/js/config.js
@@ -0,0 +1,297 @@
+var config = {
+  style: "mapbox://styles/pinkluna/cluvcscsj00vn01qj022u0ibs",
+  accessToken:
+    "pk.eyJ1IjoicGlua2x1bmEiLCJhIjoiY2l3bnoxbGo0MDAwaDJ6b2hnZnIzZjVtMyJ9.Dy-ARkRUcX0p_iVq4ZsoSg",
+  showMarkers: false,
+  markerColor: "#3FB1CE",
+  //projection: 'equirectangular',
+  //Read more about available projections here
+  //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+  inset: false,
+  theme: "dark",
+  use3dTerrain: false, //set true for enabling 3D maps.
+  auto: false,
+  footer: "",
+  chapters: [
+    {
+      id: "chapter-0",
+      alignment: "left",
+      hidden: false,
+      description: "",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.04,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "city-label",
+          opacity: 1,
+        },
+        {
+          layer: "places",
+          opacity: 0,
+        },
+        {
+          layer: "intersection",
+          opacity: 0,
+        },
+        {
+          layer: "library-points",
+          opacity: 0,
+        },
+        {
+          layer: "coffee-points",
+          opacity: 0,
+        },
+        {
+          layer: "walking-library",
+          opacity: 0,
+        },
+        {
+          layer: "walking-coffee",
+          opacity: 0,
+        },
+        {
+          layer: "intersection copy",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter-1",
+      alignment: "left",
+      hidden: false,
+      description: "This is Charlotte, North Carolina.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.04,
+        pitch: 0,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [{ layer: "city-label", opacity: 1 }],
+      onChapterExit: [
+        {
+          layer: "city-label",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter-2",
+      alignment: "left",
+      hidden: false,
+      description:
+        "My first “walkability” criteria was easy access to a library. So I found all of the <strong>public libraries</strong> and plotted them as <strong id='pink'>pink dots</strong> on the map.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "library-points",
+          opacity: 1,
+          duration: 200,
+        },
+        {
+          layer: "places",
+          opacity: 1,
+          duration: 200,
+        },
+      ],
+      onChapterExit: [],
+    },
+    {
+      id: "chapter-3",
+      alignment: "left",
+      hidden: false,
+      description:
+        "The <strong id='pink'>pink shapes</strong> are the areas of Charlotte that can be walked to in under 20 minutes from any of these libraries.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "walking-library",
+          opacity: 1,
+          duration: 200,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "walking-library",
+          opacity: 0,
+        },
+        {
+          layer: "library-points",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter-4",
+      alignment: "left",
+      hidden: false,
+      description:
+        "I did the same with <strong id='green'>coffee shops</strong> and their walkable areas.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "walking-coffee",
+          opacity: 1,
+          duration: 200,
+        },
+        {
+          layer: "coffee-points",
+          opacity: 1,
+          duration: 200,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "coffee-points",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter-5",
+      alignment: "left",
+      hidden: false,
+      description:
+        "The areas where the <strong id='pink'>library</strong> and <strong id='green'>coffee</strong> shapes overlap are places I could live where I'd be able to walk to a library <strong>and</strong> and coffee shop within 20 minutes.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "library-points",
+          opacity: 0,
+        },
+        {
+          layer: "coffee-points",
+          opacity: 0,
+        },
+        {
+          layer: "walking-library",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [
+        {
+          layer: "walking-library",
+          opacity: 0,
+        },
+        {
+          layer: "walking-coffee",
+          opacity: 0,
+        },
+      ],
+    },
+    {
+      id: "chapter-6",
+      alignment: "left",
+      hidden: false,
+      description:
+        "The intersection of these areas reveal the “most walkable” areas in Charlotte by my (totally made up) criteria.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "intersection",
+          opacity: 1,
+        },
+      ],
+      onChapterExit: [],
+    },
+    {
+      id: "chapter-7",
+      alignment: "left",
+      hidden: false,
+      mapAnimation: "flyTo",
+      description:
+        "It's clear that the largest “walkable” area of Charlotte is its center city and downtown area, called Uptown.",
+      location: {
+        center: [-80.84335, 35.22585],
+        zoom: 11.17,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [
+        {
+          layer: "intersection",
+          opacity: 1,
+        },
+        { layer: "places", opacity: 1 },
+      ],
+    },
+    {
+      id: "chapter-8",
+      alignment: "left",
+      hidden: false,
+      mapAnimation: "flyTo",
+      description:
+        "UNC Charlotte is also walkable which makes sense given that students need caffeine and books.",
+      location: {
+        center: [-80.7348, 35.30737],
+        zoom: 11.17,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+    },
+    {
+      id: "chapter-",
+      alignment: "left",
+      hidden: false,
+      mapAnimation: "flyTo",
+      description:
+        "Besides those two, there are only a handful of neighborhoods that have easy walking access to these amenities.",
+      location: {
+        center: [-80.84363, 35.20505],
+        zoom: 9.81,
+        pitch: 0,
+        bearing: 0,
+      },
+      rotateAnimation: false,
+      callback: "",
+    },
+  ],
+};
